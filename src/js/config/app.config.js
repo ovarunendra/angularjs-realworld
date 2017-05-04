18962646c27fbfc5dd0e03d743087a0dc46ee129
ovarunendra <vpsingh016@gmail.com>
2017-05-04 19:51:40 +0530

    boilerplate commit

---
+++ src/js/config/app.config.js
@@ -0,0 +1,19 @@
+function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
+  'ngInject';
+
+  /*
+    If you don't want hashbang routing, uncomment this line.
+  */
+  // $locationProvider.html5Mode(true);
+
+  $stateProvider
+  .state('app', {
+    abstract: true,
+    templateUrl: 'layout/app-view.html'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+}
+
+export default AppConfig;
