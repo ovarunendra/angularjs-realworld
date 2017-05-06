6ec50b2bcb787538e755b09f76b92d76d47dfc1d
ovarunendra <vpsingh016@gmail.com>
2017-05-06 19:35:52 +0530

    updating user settings

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -95,4 +95,18 @@ export default class User {
       return deferred.promise;
   }
 
+   // Update the current user's name, email, password, etc
+   update(fields) {
+       return this._$http({
+           url: this._AppConstants.api + '/user',
+           method: 'PUT',
+           data: { user: fields }
+       }).then(
+           (res) => {
+               this.current = res.data.user;
+               return res.data.user;
+           }
+       )
+   }
+
 }
