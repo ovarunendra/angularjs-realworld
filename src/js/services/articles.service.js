1bbfc0f82d46abe69dd800654dd097ee73171187
ovarunendra <vpsingh016@gmail.com>
2017-05-12 20:02:36 +0530

    creating the article editor

---
+++ src/js/services/articles.service.js
@@ -0,0 +1,45 @@
+export default class Articles {
+  constructor(AppConstants, $http) {
+    'ngInject';
+
+    this._AppConstants = AppConstants;
+    this._$http = $http;
+
+
+  }
+
+  // Creates or updates an article
+  save(article) {
+    let request = {};
+
+    // If there's a slug, perform an update via PUT w/ article's slug
+    if (article.slug) {
+      request.url = `${this._AppConstants.api}/articles/${article.slug}`;
+      request.method = 'PUT';
+      // Delete the slug from the article to ensure the server updates the slug,
+      // which happens if the title of the article changed.
+      delete article.slug;
+
+    // Otherwise, this is a new article POST request
+    } else {
+      request.url = `${this._AppConstants.api}/articles`;
+      request.method = 'POST';
+
+    }
+
+    // Set the article data in the data attribute of our request
+    request.data = { article: article };
+
+    return this._$http(request).then((res) => res.data.article);
+  }
+
+  // Retrieve a single article
+  get(slug) {
+    return this._$http({
+      url: this._AppConstants.api + '/articles/' + slug,
+      method: 'GET'
+    }).then((res) => res.data.article);
+  }
+
+
+}
