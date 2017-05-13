3c99a0164b247a3b56ab1b5b727db7b095f41636
ovarunendra <vpsingh016@gmail.com>
2017-05-13 08:10:13 +0530

    rendering article from server

--- src/js/article/article.controller.js
+++ src/js/article/article.controller.js
@@ -1,6 +1,16 @@
+import marked from 'marked';
+
 class ArticleCtrl {
-  constructor() {
+  constructor(article, $sce, $rootScope) {
     'ngInject';
+
+    this.article = article;
+
+    // Update the title of this page
+    $rootScope.setPageTitle(this.article.title);
+
+    // Transform the markdown into HTML
+    this.article.body = $sce.trustAsHtml(marked(this.article.body, { sanitize: true }));
 
   }
 }
--- src/js/services/articles.service.js
+++ src/js/services/articles.service.js
@@ -1,10 +1,10 @@
 export default class Articles {
-  constructor(AppConstants, $http) {
+  constructor(AppConstants, $http, $q) {
     'ngInject';
 
     this._AppConstants = AppConstants;
     this._$http = $http;
-
+    this._$q = $q;
 
   }
 
@@ -35,10 +35,23 @@ export default class Articles {
 
   // Retrieve a single article
   get(slug) {
-    return this._$http({
+    let deferred = this._$q.defer();
+
+    // Check for blank title
+    if (!slug.replace(" ", "")) {
+      deferred.reject("Article slug is empty");
+      return deferred.promise;
+    }
+
+    this._$http({
       url: this._AppConstants.api + '/articles/' + slug,
       method: 'GET'
-    }).then((res) => res.data.article);
+    }).then(
+      (res) => deferred.resolve(res.data.article),
+      (err) => deferred.reject(err)
+    );
+
+    return deferred.promise;
   }
 
 
